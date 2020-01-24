91c2482275ccbd9402052fac6237c2a8bc754191
Konstantin Shevsky <mail@shevsky.com>
2020-01-24 12:07:46 +0500

    Добавлены дополнительные параметры для ошибки со стороны API Почты: "code", "desc" и "sub-code".

--- src/Exceptions/RussianPostException.php
+++ src/Exceptions/RussianPostException.php
@@ -9,14 +9,26 @@ class RussianPostException extends \Exception
     private $raw_response = null;
 
     /**
+     * @var array
+     */
+    private $response = [];
+
+    /**
      * @var string
      */
-    private $raw_request  = null;
+    private $raw_request = null;
 
     public function __construct($message = "", $code = 0, $raw_response = null, $raw_request = null, $previous = null)
     {
         $this->raw_request = $raw_request;
         $this->raw_response = $raw_response;
+
+        $response = json_decode($this->getRawResponse(), true);
+        if ($response !== null && json_last_error() === JSON_ERROR_NONE)
+        {
+            $this->response = $response;
+        }
+
         parent::__construct($message, $code, $previous);
     }
 
@@ -50,5 +62,38 @@ class RussianPostException extends \Exception
     public function setRawRequest($raw_request)
     {
         $this->raw_request = $raw_request;
+    }
+
+    /**
+     * @return string
+     */
+    public function getErrorCode()
+    {
+        if (array_key_exists('code', $this->response))
+            return $this->response['code'];
+
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getErrorDescription()
+    {
+        if (array_key_exists('desc', $this->response))
+            return $this->response['desc'];
+
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getErrorSubCode()
+    {
+        if (array_key_exists('sub-code', $this->response))
+            return $this->response['sub-code'];
+
+        return '';
     }
 }
